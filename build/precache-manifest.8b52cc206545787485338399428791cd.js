77160e49848e5a62fb480db5bac727d357f73abe
abdirahman2ali <47459378+abdirahman2ali@users.noreply.github.com>
2021-06-15 07:48:13 -0400

    updated

---
+++ build/precache-manifest.8b52cc206545787485338399428791cd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78af5c8e6f8011e88c30160465bb04d",
+    "url": "/caev_website/index.html"
+  },
+  {
+    "revision": "c3f3c1a4cd49afe7239a",
+    "url": "/caev_website/static/css/main.3448bdf5.chunk.css"
+  },
+  {
+    "revision": "6321cfec249866cf1eda",
+    "url": "/caev_website/static/js/2.00add4d2.chunk.js"
+  },
+  {
+    "revision": "2dc0d57940f9d61efc4a165b7d2e95c1",
+    "url": "/caev_website/static/js/2.00add4d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f3c1a4cd49afe7239a",
+    "url": "/caev_website/static/js/main.0de41f00.chunk.js"
+  },
+  {
+    "revision": "7ab997f045232d9f9962",
+    "url": "/caev_website/static/js/runtime-main.d43f27e2.js"
+  },
+  {
+    "revision": "6eb50a6122bb57ff23b18548f35c9f3b",
+    "url": "/caev_website/static/media/Hero.6eb50a61.jpg"
+  },
+  {
+    "revision": "32fd3c023e113fa862b0b92a3dca47ab",
+    "url": "/caev_website/static/media/analytics.32fd3c02.png"
+  },
+  {
+    "revision": "561b3ed3b623d1ce699f797f57332392",
+    "url": "/caev_website/static/media/buffet.561b3ed3.png"
+  },
+  {
+    "revision": "2d2f75ee4760c755711ef3a85f4dcc37",
+    "url": "/caev_website/static/media/companyA.2d2f75ee.jpg"
+  },
+  {
+    "revision": "1d25c4f4c13416d93ddfb220d15ad054",
+    "url": "/caev_website/static/media/greek.1d25c4f4.png"
+  },
+  {
+    "revision": "d3f9076d4b4ef655914f45a62f2c7313",
+    "url": "/caev_website/static/media/indian.d3f9076d.png"
+  },
+  {
+    "revision": "e65a65ea1e75cdf160f923109a9833cf",
+    "url": "/caev_website/static/media/pizza.e65a65ea.png"
+  },
+  {
+    "revision": "825fb5cbee7f40d6b757a740959c1e09",
+    "url": "/caev_website/static/media/preparing.825fb5cb.png"
+  }
+]);
